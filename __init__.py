3d22ab2362ac4208893e5391486c80583bc280bd
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2022-05-11 17:22:36 -0700

    Implement Unit Tests (#4)
    * Initial test implementation
    Annotate future skill changes
    
    * Update dependencies
    
    Co-authored-by: Daniel McKnight <daniel@neon.ai>

--- __init__.py
+++ __init__.py
@@ -41,7 +41,8 @@
 # limitations under the License.
 
 from neon_utils.skills.neon_fallback_skill import NeonFallbackSkill
-from mycroft.messagebus.message import Message
+from neon_utils.logger import LOG
+from mycroft_bus_client import Message
 from threading import Lock, Event
 
 EXTENSION_TIME = 10
@@ -57,6 +58,7 @@ class QuestionsAnswersSkill(NeonFallbackSkill):
         self.query_extensions = {}  # maintains query timeout extensions
         self.lock = Lock()
         self.timeout_time = None
+        # TODO: The below params should be handled per-request
         self.waiting = Event()
         self.answered = False
 
@@ -70,25 +72,31 @@ class QuestionsAnswersSkill(NeonFallbackSkill):
             the replies.
         """
         # Check if we are certain Neon should respond to this
-        if self.neon_in_request(message) or len(str(message.data.get("utterance")).split()) >= 4:
+        if self.neon_in_request(message) or \
+                len(str(message.data.get("utterance")).split()) >= 4:
             self.answered = False
             context = message.context
             utt = message.data.get('utterance')
-            # self.enclosure.mouth_think()
             utt = utt.lower().lstrip("neon ")
+            # TODO: use UID instead of utterance DM
             self.query_replies[utt] = []
             self.query_extensions[utt] = []
             self.log.info('Searching for {}'.format(utt))
-            skill_mode = self.user_config.get('response_mode', {}).get('speed_mode', 'default')
-            extension_time = MODE_EXTENSION_TIME.get(skill_mode) or EXTENSION_TIME
+            skill_mode = self.user_config.get('response_mode',
+                                              {}).get('speed_mode', 'default')
+            extension_time = MODE_EXTENSION_TIME.get(skill_mode) or \
+                EXTENSION_TIME
             self.timeout_time = extension_time
             # Send the query to anyone listening for them
             self.waiting.clear()
-            self.bus.emit(message.forward('question:query', data={'phrase': utt}))
-            self.waiting.wait(self.timeout_time)
-            self.waiting.clear()
+            self.bus.emit(message.forward('question:query',
+                                          data={'phrase': utt}))
+            if not self.waiting.wait(self.timeout_time):
+                LOG.warning(f"timed out waiting for response to: {utt}")
+                self.waiting.clear()
 
-            self._query_timeout(Message(msg_type="neon.query_timeout", data={'phrase': utt}, context=context))
+            self._query_timeout(Message(msg_type="neon.query_timeout",
+                                        data={'phrase': utt}, context=context))
             return self.answered
         return True
 
@@ -110,13 +118,13 @@ class QuestionsAnswersSkill(NeonFallbackSkill):
                 if answer and search_phrase in self.query_replies:
                     self.log.info('Answer from {}'.format(skill_id))
                     self.query_replies[search_phrase].append(message.data)
-                    # if the confidence score is maximal, there is no need to further search for a better response
+                    # if the confidence score is maximal, stop waiting
                     if message.data.get('conf') == HIGHEST_POSSIBLE_SCORE:
                         self.waiting.set()
                 # Remove the skill from list of extensions
                 if skill_id in self.query_extensions[search_phrase]:
                     self.query_extensions[search_phrase].remove(skill_id)
-                    # if the list of extensions is empty, there are no skills left to wait for
+                    # there are no skills left to wait for
                     if not self.query_extensions[search_phrase]:
                         self.waiting.set()
             else:
@@ -151,11 +159,11 @@ class QuestionsAnswersSkill(NeonFallbackSkill):
                 # invoke best match
                 self.speak(best['answer'], message=message)
                 self.log.info('Handling with: ' + str(best['skill_id']))
-                self.bus.emit(message.forward('question:action',
-                                              data={'skill_id': best['skill_id'],
-                                                    'phrase': search_phrase,
-                                                    'callback_data':
-                                                        best.get('callback_data')}))
+                self.bus.emit(message.forward(
+                    'question:action',
+                    data={'skill_id': best['skill_id'],
+                          'phrase': search_phrase,
+                          'callback_data': best.get('callback_data')}))
                 self.answered = True
             else:
                 self.answered = False
